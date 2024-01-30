ae17d5435214facda59afc4c7a2c1c35d3bfa669
henzoparahua <hnz.2894henzop19@gmail.com>
2024-01-30 15:23:39 -0300

    black window created.

---
+++ RasterTek_Tutorial_Engine/main.cpp
@@ -0,0 +1,20 @@
+#include "systemclass.h"
+
+int WINAPI WinMain(HINSTANCE hInstace, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+{
+	SystemClass* System;
+	bool result;
+
+	System = new SystemClass;
+
+	result = System->Initialize();
+	if (result) {
+		System->Run();
+	}
+
+	System->Shutdown();
+	delete System;
+	System = 0;
+
+	return 0;
+}
---
+++ RasterTek_Tutorial_Engine/systemclass.h
@@ -0,0 +1,46 @@
+#ifndef _SYSTEMCLASS_H_
+#define _SYSTEMCLASS_H_
+
+//	Here we define WIN32_LEAN_AND_MEAN. We do this to speed up the build process.	
+//	It reduces the size of the Win32 header files by excluding some of the less used APIs.
+#define WIN32_LEAN_AND_MEAN
+
+//	We've included the headers to the other two classes in the framework at this point
+//	so we can use them in the system class.
+
+#include <windows.h>
+
+//	My class includes
+#include "inputclass.h"
+#include "applicationclass.h"
+
+class SystemClass {
+public:
+	SystemClass();
+	SystemClass(const SystemClass&);
+	~SystemClass();
+
+	bool Initialize();
+	void Shutdown();
+	void Run();
+
+	LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+	bool Frame();
+	void InitializeWindows(int&, int&);
+	void ShutdownWindows();
+	
+	LPCWSTR m_applicationName;
+	HINSTANCE m_hinstance;
+	HWND m_hwnd;
+
+	InputClass* m_Input;
+	ApplicationClass* m_Application;
+};
+
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+static SystemClass* ApplicationHandle = 0;
+
+#endif
